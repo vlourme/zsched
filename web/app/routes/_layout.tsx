05e8f5e99dbe0a6729db2a62d0e3c3bd2ebfb453
Victor Lourme <victor.lourme@icloud.com>
2025-10-28 23:28:50 +0100

    fix: responsive table

--- web/app/routes/_layout.tsx
+++ web/app/routes/_layout.tsx
@@ -16,8 +16,8 @@ export default function Layout() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <div className="p-2 bg-sidebar flex flex-col w-full">
-        <main className="flex flex-col flex-1 bg-background rounded-md">
+      <div className="w-[calc(100%-var(--sidebar-width))] flex-1 p-2 bg-sidebar flex flex-col">
+        <main className="flex flex-col flex-1 bg-background rounded-md max-w-full">
           <div className="flex items-center divide-x border-b p-3">
             <div className="pr-1">
               <SidebarTrigger />
@@ -27,9 +27,7 @@ export default function Layout() {
             </h1>
           </div>
 
-          <div>
-            <Outlet />
-          </div>
+          <Outlet />
         </main>
       </div>
     </SidebarProvider>
