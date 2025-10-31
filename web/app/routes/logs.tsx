95ecf2fc2b69e59383e61b15de97e96c07ad306a
Victor Lourme <victor.lourme@icloud.com>
2025-10-31 23:25:33 +0100

    feat(ui): add redispatch button

--- web/app/routes/logs.tsx
+++ web/app/routes/logs.tsx
@@ -1,4 +1,5 @@
-import { redirect, useLoaderData } from "react-router";
+import { Form, redirect, useLoaderData } from "react-router";
+import { Button } from "~/components/ui/button";
 import {
   Card,
   CardContent,
@@ -28,21 +29,23 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function loader({ params }: Route.LoaderArgs) {
-  const task = await pool.query(`SELECT state FROM tasks WHERE task_id = $1`, [
-    params.task_id,
+  const [task, logs] = await Promise.all([
+    pool.query(`SELECT task_name, state FROM tasks WHERE task_id = $1`, [
+      params.task_id,
+    ]),
+    pool.query(
+      `SELECT task_id, state_id, level, message, data, logged_at FROM logs WHERE task_id = $1 ORDER BY logged_at DESC
+      `,
+      [params.task_id]
+    ),
   ]);
-  const logs = await pool.query(
-    `
-    SELECT task_id, state_id, level, message, data, logged_at FROM logs WHERE task_id = $1 ORDER BY logged_at DESC
-    `,
-    [params.task_id]
-  );
 
   if (task.rows.length === 0) {
     return redirect("/tasks");
   }
 
   return {
+    task: task.rows[0].task_name,
     parameters: JSON.parse(task.rows[0].state),
     logs: logs.rows,
   };
@@ -53,19 +56,43 @@ export const handle = {
   group: "tasks",
 };
 
+export async function action({ request, params }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const taskName = formData.get("task_name");
+  const parameters = formData.get("parameters");
+
+  await fetch(process.env.ZSCHED_URL + "/tasks/" + taskName, {
+    method: "POST",
+    body: parameters as string,
+  });
+}
+
 export default function Logs() {
-  const { logs, parameters } = useLoaderData<typeof loader>();
+  const { task, logs, parameters } = useLoaderData<typeof loader>();
 
   return (
     <>
       <Card className="rounded-none bg-background">
-        <CardHeader>
-          <CardTitle>Task parameters</CardTitle>
-          <CardDescription>
-            This task was started with the following parameters.
-          </CardDescription>
+        <CardHeader className="flex flex-row justify-between items-center">
+          <div>
+            <CardTitle>{task} task parameters</CardTitle>
+            <CardDescription>
+              This task was started with the following parameters.
+            </CardDescription>
+          </div>
+          <Form method="post">
+            <input
+              type="hidden"
+              name="parameters"
+              value={JSON.stringify(parameters)}
+            />
+            <input type="hidden" name="task_name" value={task} />
+            <Button variant="outline" size="sm" type="submit">
+              Dispatch again
+            </Button>
+          </Form>
         </CardHeader>
-        <CardContent>
+        <CardContent className="max-h-96 overflow-y-auto">
           <pre className="text-sm font-mono">
             {JSON.stringify(parameters, null, 2)}
           </pre>
